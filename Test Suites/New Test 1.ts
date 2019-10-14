241d822d5a0b443cb627e370f93c9c954d2a26b0
kriss <kriss@asop.klik.digital>
2019-10-14 15:17:37 +0700

    Nambah Test suits

---
+++ Test Suites/New Test 1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b5b19649-97c9-45d2-8957-2fbc8df1a12d</testSuiteGuid>
+   <testCaseLink>
+      <guid>6045b02c-f0f2-48b0-878b-ba83c3f83656</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
